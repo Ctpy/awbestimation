28a5fa7ce272d2b78ea119cd5afd3b0b1f794ba4
ga87cer <nguyen.hu@in.tum.de>
2020-01-11 13:59:56 +0100

    change transmission_interval

--- awb_estimation.py
+++ awb_estimation.py
@@ -39,16 +39,15 @@ def estimate_available_bandwidth(target, capacity, resolution, verbose=False):
     # Numbers of packets per train
     train_length = 100
     current_ack_number = 1
+    transmission_interval = calculate_transmission_interval(transmission_rate, train_length, packet_size)
     # Probe starts here
     for i in range(12):
         print("Currently running with these Parameters: ")
-        # Send_fleet
+        utility.print_verbose("Transmission_interval: " + str(transmission_interval) + ":s", verbose)
         utility.print_verbose("Generating packet_train", verbose)
-        transmission_interval = calculate_transmission_interval(transmission_rate, train_length, packet_size)
         # if transmission_interval < globals.MIN_TRANSMISSION_INTERVAL:
         #    transmission_interval = globals.MIN_TRANSMISSION_INTERVAL
         # TODO recalc
-        utility.print_verbose("Transmission_interval: " + str(transmission_interval) + ":s", verbose)
         packet_train_numbers = generate_packet_train(current_ack_number, train_length)
         last_ack_number = packet_train_numbers[-1] + 40
         utility.print_verbose("Start transmission", verbose)
@@ -62,8 +61,9 @@ def estimate_available_bandwidth(target, capacity, resolution, verbose=False):
         utility.print_verbose("Packet_loss_rate: " + str(len(unanswered_list) / train_length), verbose)
 
         # Plot round trip times
-        plot_results(packet_train_response, round_trip_times, 'rtt{}.png'.format(i))
+        plot_results(packet_train_response, round_trip_times, 'rtt{}.png'.format(i), True)
         transmission_interval /= 2
+
         # calculate trend
 
         # trend_state, pct_trend, pdt_trend = trend.calculate_trend(timestamps, packet_loss, train_length)
@@ -82,7 +82,7 @@ def estimate_available_bandwidth(target, capacity, resolution, verbose=False):
         #     awb_max = current_awb
         # elif current_awb < awb_min:
         #     awb_min = current_awb
-        # current_ack_number = last_ack_number
+        current_ack_number = last_ack_number
         # # wait that fleets dont interfere
         # time.sleep(1)
     # Terminate and return
@@ -139,13 +139,14 @@ def calculate_transmission_interval(transmission_rate, train_length, packet_size
     return (train_length * packet_size) / transmission_rate
 
 
-def plot_results(packet_train_response, round_trip_times, filename='rtt.png', ):
+def plot_results(packet_train_response, round_trip_times, filename='rtt.png', clear=False):
     mp.plot(np.array(range(len(packet_train_response))), np.array(round_trip_times))
     mp.ylabel("Round trip time in second")
     mp.xlabel("Packet index")
     mp.savefig(filename, format='png')
     mp.show()
-
+    if clear:
+        mp.clf()
 
 if __name__ == '__main__':
     estimate_available_bandwidth(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), True)
